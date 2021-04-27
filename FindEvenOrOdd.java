c55cda24b0e7a2dc44dbc559222b209866ae4534
DemoProject-Core <82090750+DemoProject-Core@users.noreply.github.com>
2021-04-27 17:56:59 +0530

    Add files via upload

---
+++ FindEvenOrOdd.java
@@ -0,0 +1,22 @@
+public class FindEvenOrOdd {
+
+    public static void findNumber() {
+
+for (int i,i = 1 to 10 , i++){
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("ODD");
+        }
+	i= i+1;
+}
+
+    }
+
+    public static void main(String[] args) {
+
+        findNumber();
+
+    }
+
+}
